b954dfb685cef082c1fd91789b724b6c4da793cf
Ivan Starovoytov <vs228228@gmail.com>
2024-08-28 13:23:38 +0300

    Add Paginations and validators

---
+++ EventsWebApplication.Server/Application/Validators/UserCreateDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using EventsWebApplication.Server.Application.DTOs;
+using FluentValidation;
+
+namespace EventsWebApplication.Server.Application.Validators
+{
+    public class UserCreateDtoValidator : AbstractValidator<UserCreateDto>
+    {
+        public UserCreateDtoValidator()
+        {
+            RuleFor(x => x.Birthday)
+                .SetValidator(new DateOnlyDtoValidator());
+        }
+    }
+    
+    
+}
---
+++ EventsWebApplication.Server/Application/Validators/UserUpdateDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using EventsWebApplication.Server.Application.DTOs;
+using FluentValidation;
+
+namespace EventsWebApplication.Server.Application.Validators
+{
+    public class UserUpdateDtoValidator : AbstractValidator<UserUpdateDto>
+    {
+        public UserUpdateDtoValidator()
+        {
+            RuleFor(x => x.Birthday)
+                .SetValidator(new DateOnlyDtoValidator());
+        }
+    }
+}
